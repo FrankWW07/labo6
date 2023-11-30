86ba496f2d814ac1654b257b0703b659728693d3
Frank <117088885+FrankWW07@users.noreply.github.com>
2023-11-30 17:55:18 -0600

    Crear dataManager.py y se añadió paquetes necesarios, parte de código para poder imprimir los datos actualizados y usar el archivo ''eventos.py''

---
+++ dataManager.py
@@ -0,0 +1,53 @@
+import time
+import random
+import threading
+from eventos import EventManager # Importamos la clase EventManager del archivo eventos.py
+
+class RealTimeDataManager:
+  def __init__(self):
+    self.data = {"temperatura": 25.0, "humedad": 60.0}
+    self.event_manager = EventManager()
+    # variable para controlar la ejecución del programa
+    self.running = True
+
+  def start_real_time_updates(self):
+    while self.running:
+      # el bucle se ejecutará mientras running sea True
+      time.sleep(3)
+      self.generate_real_time_data()
+      # Notificar al EventManager que los datos han cambiado
+      self.event_manager.notify('datos_actualizados', self.data)
+
+  def generate_real_time_data(self):
+      self.data["temperatura"] += random.uniform(-1.0, 1.0)
+      self.data["humedad"] += random.uniform(-2.0, 2.0)
+
+  def stop_updates(self):
+    # cambia el estado de running para detener el bucle
+    self.running = False
+
+
+# Creamos una instancia de RealTimeDataManager
+real_time_data_manager = RealTimeDataManager()
+
+# Función de callback para imprimir los datos actualizados
+def imprimir_datos_actualizados(data):
+  print("Datos actualizados:")
+  print(f"Temperatura: {data['temperatura']} °C")
+  print(f"Humedad: {data['humedad']} %")
+  print("-----------")
+
+# Suscribimos el callback al EventManager para el evento 'datos_actualizados'
+real_time_data_manager.event_manager.subscribe('datos_actualizados', imprimir_datos_actualizados)
+
+# Actualizaciones en tiempo real en segundo plano
+update_thread = threading.Thread(target=real_time_data_manager.start_real_time_updates)
+update_thread.start()
+
+try:
+  while True:
+    time.sleep(1)
+except KeyboardInterrupt:
+  # llama a la función para detener las actualizaciones
+  real_time_data_manager.stop_updates()
+  print("\nPrograma terminado.")
